a1461178c0de7d7e28827e95dba0ed0e531c0cbc
Abhigyan Trips <abhigyantrips@gmail.com>
2023-02-22 19:10:50 +0530

    feat: enable vercel analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,16 @@
-import "@/styles/globals.css"
-import "@/styles/notion.css"
-import Layout from "@/components/Layout"
+import { Analytics } from "@vercel/analytics/react";
+
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
+import "@/styles/notion.css";
 
 export default function App({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	)
-}
+		<>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+			<Analytics />
+		</>
+	);
+};
