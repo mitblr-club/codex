e84cf5d3e723cada1035e8c7dc66e0271900408b
Abhigyan Trips <abhigyantrips@gmail.com>
2023-02-22 09:26:21 +0530

    feat!: update events, revalidate props

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -15,6 +15,7 @@ export async function getStaticProps() {
 		props: {
 			posts
 		},
+		revalidate: 60,
 	};
 }
 
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,5 +1,6 @@
 import siteConfig from "@/site.config";
 import Link from "next/link";
+import { formatDate } from "@/lib/formatDate";
 
 export async function getAllPosts() {
 	return await fetch(
@@ -8,25 +9,51 @@ export async function getAllPosts() {
 };
 
 export async function getStaticProps() {
-  const posts = await getAllPosts()
+	const posts = await getAllPosts()
 
-  return {
-    props: {
-      posts
-    },
-  };
+	return {
+		props: {
+			posts
+		},
+		revalidate: 60,
+	};
 }
 
 export default function EventsHome({ posts }) {
-  return (
-    <div className="p-2">
-      {posts.filter((post) => {
-        return (post.published ? post : null)
-      }).map((post) => (
-        <Link href="/events/[slug]" as={`/events/${post.slug}`} key={post.title}>
-          <h1 className="text-white">{post.title}</h1>
-        </Link>
-      ))}
-    </div>
-  );
+	return (
+		<>
+			<h1 className="notion notion-title">CodeX Events</h1>
+			<div className="notion notion m-12 grid gap-10 lg:grid-cols-2">
+				{posts.filter((post) => {
+					return (post.published ? post : null)
+				}).sort((post) => {
+					return (formatDate(post.date))
+				}).map((post) => (
+					<article
+						key={post.title}
+						className="block p-6 bg-gray-50 border border-gray-300 rounded-lg shadow-md group"
+					>
+						<Link 
+							href="/events/[slug]" 
+							as={`/events/${post.slug}`} 
+							key={post.title} 
+							passHref
+						>
+						<div>
+							<h2 className="mb-2 text-xl font-bold tracking-tight text-gray-900 group-hover:text-primary">
+								{post.title}
+							</h2>
+							{post.date ? (
+								<p className="my-4 text-gray-500">{formatDate(post.date)}</p>
+							) : null}
+							{post.description ? (
+								<p className="my-4 py-3 text-gray-700">{post.description}</p>
+							) : null}
+						</div>
+						</Link>
+					</article>
+				))}
+			</div>
+		</>
+	);
 }
