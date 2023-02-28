4623161b6b809b2b14091c36ba4e99c58689a397
Abhigyan Trips <abhigyantrips@gmail.com>
2023-02-28 23:22:28 +0530

    feat: init author, dept data

---
+++ data/authors.js
@@ -0,0 +1,62 @@
+import departments from "./departments"
+
+module.exports = { 
+	"18fd9266-d780-4278-af73-e63b86a4569e": {
+	  firstName: "Abhigyan",
+	  lastLame: "Tripathi",
+	  fullName: "Abhigyan Tripathi",
+	  department: departments.cxsd,
+	  profilePhoto: "https://lh3.googleusercontent.com/a-/AOh14GhOyMo82QamFa_yADhOcmwqi7VTg0jOXGsPs6LC=s100"
+	},
+	"0a78f39d-eb87-4653-b2a2-872f13be5cba": {
+	  fullName: "Nakul Kamath",
+	  department: departments.cxsmm,
+	  profilePhoto: "https://lh3.googleusercontent.com/a/AEdFTp6nEaBgNPWls1Ln8ClJg5nFqHWkAJ9t2hArvvE-2Q=s100"
+	},
+	"0d42484b-296a-46d4-94d8-d760f77ae7b8": {
+	  firstName: "Kevin",
+	  lastLame: "Thomas",
+	  fullName: "Kevin Thomas",
+	  department: departments.cxad
+	},
+	"7bd0a6bf-ba28-4595-a12c-9ed33b7fa150": {
+	  fullName: "Ojas Joshi",
+	  department: departments.cxcp,
+	  profilePhoto: "https://lh3.googleusercontent.com/a-/AFdZucoWWapihwOBSJoFvEAGc4KJUYW8-pbaSRd2oGOhJw=s100"
+	},
+	"fadff20c-3866-4c9f-893c-e6e3f824dd5c": {
+	  fullName: "Mayukh Saha",
+	  department: departments.cxcp,
+	  profilePhoto: "https://lh3.googleusercontent.com/a-/AOh14Gj9vkSd6fbqpcPuuVo5BrprOfm6pG2DmylAZEvHIw=s100"
+	},
+	"3df21d65-c8b5-4bba-8b3b-632be070c647": {
+	  fullName: "Vaishnavi D",
+	  department: departments.cxsmm,
+	  profilePhoto: "https://s3-us-west-2.amazonaws.com/public.notion-static.com/061bbe1d-1847-4a51-a39b-164aebd74538/B612_20230214_212218_301.jpg"
+	},
+	"46ef6cad-ce53-43b4-8470-b044755d043d": {
+	  fullName: "Sherone D'Souza"
+	},
+	"21a2fe58-fefc-44e0-a056-1866679c1f41": {
+	  fullName: "Astitva Agarwal",
+	  department: departments.cxos,
+	  profilePhoto: "https://lh3.googleusercontent.com/a/AEdFTp7PsdRlAajhIQPBNxovVu_fx76eMEztDovQAnU-pg=s100"
+	},
+	"6305169d-3452-43fb-934c-fed4320ee111": {
+	  firstName: "Deepro",
+	  lastLame: "Chakravorty",
+	  fullName: "Deepro Chakravorty",
+	  department: departments.cxgd,
+	  profilePhoto: "https://lh3.googleusercontent.com/a-/AOh14Gjd-8YNQOj1UhhyHwm1YVodT_5eDpioHFjQdoLaMQ=s100"
+	},
+	"f32e0565-ca68-4a58-bd3a-ae350bc1ae95": {
+	  firstName: "Chandan",
+	  lastLame: "CV",
+	  fullName: "Chandan CV",
+	  department: departments.cxos,
+	  profilePhoto: "https://lh3.googleusercontent.com/a-/AOh14GhI9GiRFJqxFfwS_HA4KopvdZ2xtMHQ9IQtgoenD3Y=s100"
+	},
+	"efef3c0b-1ef9-4527-9046-a61d32eed27a": {
+	  fullName: "Rochak Saini"
+	}
+}
---
+++ data/departments.js
@@ -0,0 +1,32 @@
+module.exports = {
+	"cxsd": {
+		name: "Software Development",
+		description: "deals with the topics related to generalized software development - this includes APIs, PWAs, libraries and even cross-platform applications.",
+		shorthand: "CXSD"
+	},
+	"cxcp": {
+		name: "Competitive Programming",
+		description: "This is the description of competitive programming.",
+		shorthand: "CXCP"
+	},
+	"cxos": {
+		name: "Open-Source Development",
+		description: "is responsible for maintaining, creating, and promoting open-source projects from CodeX and other public repositories.",
+		shorthand: "CXOS"
+	},
+	"cxgd": {
+		name: "Game Development",
+		description: "explores all aspects of game devevelopment; be it programming, 3D modelling or sound design.",
+		shorthand: "CXGD"
+	},
+	"cxit": {
+		name: "Information Technology",
+		description: "This is the description of competitive programming.",
+		shorthand: "CXIT"
+	},
+	"cxsmm": {
+		name: "Social Media Management",
+		description: "This is the description of competitive programming.",
+		shorthand: "CXSMM"
+	}
+}
