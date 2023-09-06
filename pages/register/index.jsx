1475e64c2de3d22ce7496ac82864eef5fa7eaa03
Abhigyan Tripathi <abhigyantrips@gmail.com>
2023-09-06 13:57:02 +0530

    feat: add meta content to `/register`

--- pages/register/index.jsx
+++ pages/register/index.jsx
@@ -1,9 +1,27 @@
 import { useEffect } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
+import siteConfig from "@/site.config";
 
 export default function Register() {
 	const router = useRouter();
 	useEffect(() => {
 		router.replace("https://forms.office.com/r/ugiEExigqe");
 	});
+
+	return (
+		<Head>
+			<title>{`Registrations | ${siteConfig.headerTitle}`}</title>
+			<meta
+				name="og:title"
+				content={`Registrations | ${siteConfig.headerTitle}`}
+			/>
+			<meta name="og:description" content={siteConfig.description} />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1"
+			/>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+	);
 }
