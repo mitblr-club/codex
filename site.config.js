eb84d6f1afe6be101dc80923163aa720dc3f333d
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-01-20 10:19:46 +0530

    chore: update event and blog IDs

--- site.config.js
+++ site.config.js
@@ -7,8 +7,8 @@ const siteConfig = {
 
 	siteUrl: "https://codex.mitblr.club/",
 	siteLogo: "/public/logo.png",
-	eventsTableId: "bce097af99dc4179983660dbafb479fc",
-	blogTableId: "bc59e12441b14107880e8e479d89ea50",
+	eventsTableId: "df0bbd9604a846468e6aaf7f41042c1c",
+	blogTableId: "4388cd358b214e539f9c5ea5f941ad4e",
 
 	navLinks: [
 		{ href: "/events", title: "Events" },
